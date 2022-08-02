f1081599736a7119487a9ba71206ceb0150e2b66
Emmijozzy <josephogunsuyi360@gmail.com>
2022-08-02 16:53:33 +0000

    first commit

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   google: {
-    API_KEY: "",
-    CLIENT_ID: "",
-    SECRET: "",
+    API_KEY: "AIzaSyBq71YhH84V_VcdLFvS_kPs6ovAgxP8Kk4",
+    CLIENT_ID: "760166332870-qrdk2fh14flo81e0lgvn4gj0om0l851g.apps.googleusercontent.com",
+    SECRET: "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",
   },
   facebook: {
-    APP_ID: "",
+    APP_ID: " 443994917652017",
   },
   api: {
     API_URL: "https://api-node.themesbrand.website",
